63f63650d1585863c61103f336adabe35e8521a5
João Pster (Dell) <joaopsterdev@gmail.com>
2024-11-03 19:28:41 -0300

    test: coverage remove

--- src/lambdas/createAuthChallenge/jest.config.js
+++ src/lambdas/createAuthChallenge/jest.config.js
@@ -8,15 +8,4 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   clearMocks: true,
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['/node_modules/', '/__tests__/'],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
 };
